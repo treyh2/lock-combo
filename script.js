98299b63afa7571e771809290da9faf1999df654
Your Name <your_email@example.com>
2024-01-19 09:41:23 -0600

    added .js and .html with finished model

---
+++ script.js
@@ -0,0 +1,15 @@
+// Declaring a variable called message and assigning it to a string.
+
+let message = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:"
+
+// Declaring 3 variables: code1, code2, & code3 then assign their values using operators.
+
+let code1 = 11-1;
+let code2 = 4*10;
+let code3 = 35+4;
+
+// Declare variable combination and assign the values of code1-3 with dashes to separate the codes.
+let combination = code1 + "-" + code2 + "-" + code3;
+
+// Create alert() function to display the message and the combination in an alert box.
+alert(message + "\n" + combination);
